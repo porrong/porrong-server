c55581f19802c2878fd66d536a44da88ea7a1bc0
이승윤 <yyuunn17@naver.com>
2022-03-10 14:09:16 +0900

    ⚙️ :: init

---
+++ src/main/java/com/syxxn/porrongserver/PorrongServerApplication.java
@@ -0,0 +1,13 @@
+package com.syxxn.porrongserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorrongServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorrongServerApplication.class, args);
+	}
+
+}
