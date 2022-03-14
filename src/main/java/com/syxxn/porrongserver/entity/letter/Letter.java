983361c1629c530c55fb67056d08636e92d5c143
이승윤 <yyuunn17@naver.com>
2022-03-14 09:27:02 +0900

    📝 :: letter entity 추가

---
+++ src/main/java/com/syxxn/porrongserver/entity/letter/Letter.java
@@ -0,0 +1,45 @@
+package com.syxxn.porrongserver.entity.letter;
+
+import com.syxxn.porrongserver.entity.BaseIdEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_letter")
+@Entity
+public class Letter  extends BaseIdEntity {
+
+    @Column(nullable = false, length = 20000)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime releaseDate;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, columnDefinition = "bit default 0")
+    private Boolean isReleased = false;
+
+    public static Letter createLetter(String content, LocalDateTime releaseDate) {
+        Letter letter = new Letter();
+        letter.content = content;
+        letter.releaseDate = releaseDate;
+
+        return letter;
+    }
+
+    public void isReleasedTrue() {
+        this.isReleased = true;
+    }
+
+}
---
+++ src/main/java/com/syxxn/porrongserver/entity/letter/repository/LetterRepository.java
@@ -0,0 +1,7 @@
+package com.syxxn.porrongserver.entity.letter.repository;
+
+import com.syxxn.porrongserver.entity.letter.Letter;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LetterRepository extends CrudRepository<Letter, Long> {
+}
