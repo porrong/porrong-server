b73ab06989950d071daed40ae71c06137a15bdf7
이승윤 <yyuunn17@naver.com>
2022-03-17 13:41:14 +0900

    📑 :: ControllerAdvice 클래스 추가

---
+++ src/main/java/com/syxxn/porrongserver/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.syxxn.porrongserver.exception.handler;
+
+import com.syxxn.porrongserver.exception.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //정의된 모든 에러 처리
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(final GlobalException e) {
+        log.error(e.getCause().getMessage());
+        return new ResponseEntity<>(new ErrorResponse(e.getStatusCode(), e.getLocalizedMessage()), HttpStatus.valueOf(e.getStatusCode()));
+    }
+
+    //@Valid 어노테이션을 통과하지 못할 때
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        log.error(e.getCause().getMessage());
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getLocalizedMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    //request에서 직접 받은 enum값이 잘못 되었을 때
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
+        log.error(e.getCause().getMessage());
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getLocalizedMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    //데이터 무결성이 위반되었을 때
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseEntity<ErrorResponse> handleConflictException(final DataIntegrityViolationException e) {
+        log.error(e.getCause().getMessage());
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.CONFLICT.value(), e.getLocalizedMessage()), HttpStatus.CONFLICT);
+    }
+
+    //NPE
+    @ExceptionHandler(NullPointerException.class)
+    protected ResponseEntity<ErrorResponse> handleNullPointerException(final NullPointerException e) {
+        log.error(e.getCause().getMessage());
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
