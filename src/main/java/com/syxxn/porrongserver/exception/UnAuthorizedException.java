fb3901076d01d0ce09690062dd93e455b99c2fc7
이승윤 <yyuunn17@naver.com>
2022-03-18 10:04:53 +0900

    ⚡️ :: exception 추가

--- src/main/java/com/syxxn/porrongserver/exception/UnAuthorizedException.java
+++ src/main/java/com/syxxn/porrongserver/exception/UnAuthorizedException.java
@@ -7,6 +7,8 @@ public class UnAuthorizedException extends GlobalException {
 
     public static final UnAuthorizedException EXCEPTION = new UnAuthorizedException();
 
+    public static final UnAuthorizedException INVALID_EXPIRED_TOKEN = new UnAuthorizedException("Invalid Or Expired Token.");
+
     public static final UnAuthorizedException NOT_ACCESSIBLE = new UnAuthorizedException("User Not Accessible.");
 
     private UnAuthorizedException() {
