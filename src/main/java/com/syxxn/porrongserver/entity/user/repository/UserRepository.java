4a2f2cd09ebda899d1dbf0a1633125ced4fd27b1
이승윤 <yyuunn17@naver.com>
2022-03-18 10:03:27 +0900

    ⚡️ :: 기능 업데이트

--- src/main/java/com/syxxn/porrongserver/entity/user/repository/UserRepository.java
+++ src/main/java/com/syxxn/porrongserver/entity/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.syxxn.porrongserver.entity.user.repository;
 import com.syxxn.porrongserver.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
