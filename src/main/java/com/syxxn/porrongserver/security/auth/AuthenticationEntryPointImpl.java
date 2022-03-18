5549c855869a32aa4990b6596a5e961c7e24a825
이승윤 <yyuunn17@naver.com>
2022-03-18 10:12:16 +0900

    ⚙️ :: AuthenticationEntryPointImpl

---
+++ src/main/java/com/syxxn/porrongserver/security/auth/AuthenticationEntryPointImpl.java
@@ -0,0 +1,19 @@
+package com.syxxn.porrongserver.security.auth;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Check the token"); // 토큰의 형식이 잘못된 경우 400
+    }
+
+}
