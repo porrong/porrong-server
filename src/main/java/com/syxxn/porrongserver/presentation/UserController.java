feb39a09090c818ff5954cf9026db4c88c646dff
이승윤 <yyuunn17@naver.com>
2022-04-28 14:36:50 +0900

    🐛 :: 카카오 로그인 시 request로 묶기

--- src/main/java/com/syxxn/porrongserver/presentation/UserController.java
+++ src/main/java/com/syxxn/porrongserver/presentation/UserController.java
@@ -1,13 +1,17 @@
 package com.syxxn.porrongserver.presentation;
 
+import com.syxxn.porrongserver.presentation.dto.request.KakaoLoginRequest;
 import com.syxxn.porrongserver.security.jwt.TokenResponse;
 import com.syxxn.porrongserver.service.user.DeleteUserService;
 import com.syxxn.porrongserver.service.user.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -18,8 +22,8 @@ public class UserController {
     private final DeleteUserService deleteUserService;
 
     @PostMapping
-    public TokenResponse login(String code) {
-        return loginService.execute(code);
+    public TokenResponse login(@RequestBody @Valid KakaoLoginRequest request) {
+        return loginService.execute(request.getCode());
     }
 
     @DeleteMapping
