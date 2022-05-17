3efe3d8db00faf19eff24a8a113ced75951933e8
이승윤 <yyuunn17@naver.com>
2022-05-17 14:52:50 +0900

    ⚡️ :: Spring boot Schedule 추가
    isReleasedTrue 메소드 실행

---
+++ src/main/java/com/syxxn/porrongserver/batch/SetIsReleased.java
@@ -0,0 +1,25 @@
+package com.syxxn.porrongserver.batch;
+
+import com.syxxn.porrongserver.entity.letter.Letter;
+import com.syxxn.porrongserver.entity.letter.repository.LetterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Component
+public class SetIsReleased {
+
+    private final LetterRepository letterRepository;
+
+    @Transactional
+    @Scheduled(cron = "0 0 0 * * *") //매일 00시 00분 00초에 실행
+    public void setIsReleased() {
+        letterRepository.findAllByReleaseDateGreaterThanEqual(LocalDateTime.now())
+                .forEach(Letter::isReleasedTrue);
+    }
+
+}
