96768efda47ca79203b64ef829cf877af5444c02
이승윤 <yyuunn17@naver.com>
2022-05-19 12:02:49 +0900

    🐛 :: CORS 해결

--- src/main/java/com/syxxn/porrongserver/config/WebMvcConfig.java
+++ src/main/java/com/syxxn/porrongserver/config/WebMvcConfig.java
@@ -2,10 +2,8 @@ package com.syxxn.porrongserver.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -13,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("OPTIONS", "GET", "POST")
+                .allowedMethods("*")
                 .allowedHeaders("*");
     }
 
