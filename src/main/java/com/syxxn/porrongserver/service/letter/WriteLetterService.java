817dd7e7f6d854cb43f5bce47657bde7c97b6d97
이승윤 <yyuunn17@naver.com>
2022-06-10 11:04:29 +0900

    ♻️ :: 사용하지 않는 import 삭제

--- src/main/java/com/syxxn/porrongserver/service/letter/WriteLetterService.java
+++ src/main/java/com/syxxn/porrongserver/service/letter/WriteLetterService.java
@@ -9,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-
 @RequiredArgsConstructor
 @Transactional
 @Service
